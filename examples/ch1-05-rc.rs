286d72df66c55e10f67509a43d7fef068f22998c
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-10-09 10:12:23 -0700

    arc example

--- examples/ch1-05-rc.rs
+++ examples/ch1-05-rc.rs
@@ -1,8 +1,16 @@
-use std::rc::Rc;
+use std::{rc::Rc, sync::Arc, thread};
 
 fn main() {
     let a = Rc::new([1, 2, 3]);
     let b = a.clone();
 
     assert_eq!(a.as_ptr(), b.as_ptr()); // Same allocation!
+
+    let a = Arc::new([1, 2, 3]);
+    let b = a.clone();
+
+    let at = thread::spawn(move || dbg!(a));
+    let bt = thread::spawn(move || dbg!(b));
+    at.join().unwrap();
+    bt.join().unwrap();
 }
