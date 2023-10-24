13e4aa0446e794740835d7205043bbc63ae24118
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-10-24 10:53:16 -0700

    oredering notes

--- examples/ch3-03-total-modification-order.rs
+++ examples/ch3-03-total-modification-order.rs
@@ -5,6 +5,7 @@ use std::thread;
 static X: AtomicI32 = AtomicI32::new(0);
 
 fn a() {
+    // There is a predictable modification order that happens within the same thread
     X.fetch_add(5, Relaxed);
     X.fetch_add(10, Relaxed);
 }
@@ -17,12 +18,14 @@ fn b() {
     println!("{a} {b} {c} {d}");
 }
 
-// Modification order of a variable is seen consistently across all the threads
+// Relaxed memory order: modificasions of a single variable are seen consistently across all the threads
 // Possible output:   0-0-0-0   0-0-5-15   0-15-15-15
 // Impossible output: 0-5-0-15  0-0-10-15
 fn main() {
     thread::scope(|s| {
+        // All that happened before the spawn is visible to the thread
         s.spawn(a);
         s.spawn(b);
+        // All that happened after the join is visible to the thread
     });
 }
