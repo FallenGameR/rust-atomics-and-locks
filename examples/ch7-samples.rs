31c8a9270790fec71606d21177a66f6fe7a06b35
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-10-30 12:15:21 -0700

    read modify write example

--- examples/ch7-samples.rs
+++ examples/ch7-samples.rs
@@ -3,10 +3,12 @@ https://godbolt.org/
 rustc 1.73
 -O
 -O --target=aarch64-unknown-linux-musl
-
 */
 use std::sync::atomic::AtomicI32;
 use std::sync::atomic::Ordering::{Relaxed, Acquire, Release, AcqRel, SeqCst};
+
+fn main() {
+}
 
 /*
 
@@ -73,6 +75,9 @@ pub fn load(x: &i32) -> i32 {
 /*
 
 example::load_relaxed:
+  ; It is the same atomic operation for processor,
+  ; but not for the compiler. The compiler could have
+  ; split this operation into two that use 16-bit registers
   mov eax, dword ptr [rdi]
   ret
 
@@ -85,5 +90,33 @@ pub fn load_relaxed(x: &AtomicI32) -> i32 {
     x.load(Relaxed)
 }
 
-fn main() {
+/*
+
+; X64 is CISC architecture - complex instruction set computer
+example::read_modify_write:
+  ; although it looks atomic, it is actually not
+  ; the processor would split this `add` operation
+  ; into several microinstructions similar to RISC
+  ;
+  ; that would be save on a single core CPU though since
+  ; the context switches happen only betweern instructions,
+  ; not between microinstructions
+  ;
+  ; but modern CPUs are muti-core and there we don't
+  ; have such guarantee
+  add dword ptr [rdi], 10
+  ret
+
+; AARCH64 is RISC architecture - reduced instruction set computer
+example::read_modify_write:
+  ldr w8, [x0]
+  ; [x0] can be modified by another thread here
+  add w8, w8, #10
+  ; and here
+  str w8, [x0]
+  ret
+
+*/
+pub fn read_modify_write(x: &mut i32) {
+    *x += 10;
 }
