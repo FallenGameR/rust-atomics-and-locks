c37224b1de4b89df53dc252c914b835619d76535
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-11-10 12:08:29 -0800

    better Release Aquire explanation

--- examples/ch3-06-release-acquire.rs
+++ examples/ch3-06-release-acquire.rs
@@ -6,16 +6,36 @@ use std::thread;
 use std::time::Duration;
 
 static DATA: AtomicU64 = AtomicU64::new(0);
+
+// Like a gate to the DATA
 static READY: AtomicBool = AtomicBool::new(false);
 
 fn main() {
     thread::spawn(|| {
         DATA.store(123, Relaxed);
+
+        // A Release operation may not be reordered
+        // with any memory operations that precede it.
+        //
+        // CPU uses internal buffers and async operations
+        // for executing individual instructions. From a
+        // single core perspective everything would seem
+        // to be in order, but the other cores may see
+        // that instructions complete in a different order
+        // of how they were started.
+        //
+        // Memory ordering can cause CPU to flush it's
+        // internal buffers and complete all the operations
+        // that still aer unfinished in the pipeline and
+        // only then to proceed with the next instruction.
+        //
         READY.store(true, Release); // Everything from before this store ..
     });
 
-    // Like a gate
 
+    // An Acquire operation may not be reordered
+    // with any memory operations that follow it.
+    //
     while !READY.load(Acquire) { // .. is visible after this loads `true`.
         thread::sleep(Duration::from_millis(100));
         println!("waiting...");
