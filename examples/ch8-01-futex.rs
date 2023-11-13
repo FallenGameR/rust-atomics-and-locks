afd838b25b55a30eef6b783fee88330cc95f40ea
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-11-13 12:39:06 -0800

    ongoing

--- examples/ch8-01-futex.rs
+++ examples/ch8-01-futex.rs
@@ -1,4 +1,5 @@
 // We don't have that capability in Windows, this is why Rust analyzer can't see main here.
+// Windows have a similar functionality that allows to wait on changes of a memory address.
 #![cfg(target_os = "linux")]
 
 use std::sync::atomic::AtomicU32;
@@ -31,6 +32,14 @@ pub fn wake_one(a: &AtomicU32) {
     }
 }
 
+// This is essentially thread parkin/unparking implementation
+// in Rust on Linux before 1.48. This kind of synchronization
+// is fast because if we don't need to wait no syscall is
+// being made since we track all the state in user mode ourselves.
+//
+// For park/unpark Rust stores a AtomicU32 variable per thread
+// that has 3 states: 1) init/idle 2) unparked 3) parked.
+
 fn main() {
     let a = AtomicU32::new(0);
 
@@ -43,6 +52,10 @@ fn main() {
 
         println!("Waiting...");
         while a.load(Relaxed) == 0 {
+            // Ocasionally it's possible that the wait is interrupted
+            // and the thread wakes up but actually the variable was
+            // not really modified. So we need a loop that checks the
+            // value of a. This is similar to the thread park/unpark.
             wait(&a, 0);
         }
         println!("Done!");
