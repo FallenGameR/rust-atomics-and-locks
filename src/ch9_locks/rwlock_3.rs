5ba028a3ffb20b413ea77c43cd62980365b3164d
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-11-22 12:31:28 -0800

    figuring out writer priority

--- src/ch9_locks/rwlock_3.rs
+++ src/ch9_locks/rwlock_3.rs
@@ -5,11 +5,11 @@ use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 
 pub struct RwLock<T> {
-    /// The number of read locks times two, plus one if there's a writer waiting.
-    /// u32::MAX if write locked.
+    /// The number of read locks times two, plus one if there's
+    /// a writer waiting, u32::MAX if write locked.
     ///
-    /// This means that readers may acquire the lock when
-    /// the state is even, but need to block when odd.
+    /// This means that readers may acquire the lock only
+    /// when the state is even, but need to block when odd.
     state: AtomicU32,
     /// Incremented to wake up writers.
     writer_wake_counter: AtomicU32,
